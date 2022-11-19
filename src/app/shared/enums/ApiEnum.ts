b0f5230f453fb4723bdb71bb9cd8d4a88ba40e11
Thi Thanh Tuyen Nguyen <tnguyen7s@semo.edu>
2022-11-19 13:11:50 -0600

    fix bug in postService

--- src/app/shared/enums/ApiEnum.ts
+++ src/app/shared/enums/ApiEnum.ts
@@ -4,7 +4,7 @@ export enum ApiEnum {
   Account ='api/account',
   RequestReset = 'api/account/resetPassRequest?email=',
   ResetPassword = 'api/account/resetPassword',
-  Post = 'api/post',
+  Post = 'api/post/',
   FilterPostByTag = 'api/post/filter?tag=',
   UsersPosts =  'api/post/user',
   PostContacts = 'api/post/contact/'
