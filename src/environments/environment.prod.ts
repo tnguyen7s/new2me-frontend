d59df5f735ad49c522530bfbcb87f7a1948c6afe
Thi Thanh Tuyen Nguyen <tnguyen7s@semo.edu>
2022-11-19 13:17:00 -0600

    recapcha

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://new2me-webapi.herokuapp.com/',
-  recapchaSiteKey: '6Lcn0hwjAAAAAHk8yGkbzCls0RiBMZkJ2J5FRba4'
+  recapchaSiteKey: '6Lcv1hwjAAAAAJ82ezpGsZMNkJTy_k2kevDhcy0x'
 };
